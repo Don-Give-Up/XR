6c74867e9cd1adffedeaed382e28372e54af47b2
MinJuuu91923 <qkralswn306@gmail.com>
2024-10-23 10:00:59 +0900

    도토리

--- 4Don/Assets/PMJ/Scripts/Quiz.cs
+++ 4Don/Assets/PMJ/Scripts/Quiz.cs
@@ -1,30 +1,44 @@
 using System;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 
 public class Quiz : MonoBehaviour
 {
     public QuizlistData quizlistdata;
-    
+
     public TMP_Text TextquizNum;
     public TMP_Text Textcategory;
     public TMP_Text Textquiz;
-    
+
+    public GameObject sugoimage;
+
+    public Button oButton;
+    public Button xButton;
+
+    private int correntAnswerCount = 0; // 맞힌 정답 갯수
+    private List<int> usedEasyQuestions = new List<int>(); // 이미 출제된 문제 기록
 
     private int _easyCount;
     private int _normalCount;
-    private int _hardCoont;
+    private int _hardCount;
 
     private void Start()
     {
         QuizStart();
         ShowEasyQuiz();
+        sugoimage.SetActive(false);
+
+        // O 버튼과 X 버튼에 정답 체크 이벤트 연결
+        oButton.onClick.AddListener(() => OnAnswerSelected("O"));
+        xButton.onClick.AddListener(() => OnAnswerSelected("X"));
     }
 
-    public void QuizStart()
+    public void QuizStart() // 퀴즈 먼저 읽어오기
     {
         //폴더에서 json파일 찾아
         TextAsset jsonQuizData = Resources.Load<TextAsset>("Quiz");
@@ -53,15 +67,20 @@ public class Quiz : MonoBehaviour
         }
         //QuizlistData loadedData = JsonConvert.DeserializeObject<QuizlistData>();
     }
-    
-    private QuizData GETEasyQuiz()
-    {
-        if (quizlistdata != null)
-        {
-            
-            _easyCount = quizlistdata.easy.Count;
 
-            int easyRandom = Random.Range(0, _easyCount);
+    private QuizData GETEasyQuiz() // easy난이도를 선택했을때 나옴, 이건 처음에 1문제만 나오지만 정답이 5개가 될때까지 나오도록 고쳐야함.
+    {
+        _easyCount = quizlistdata.easy.Count;
+        if (quizlistdata != null && _easyCount > 0 && usedEasyQuestions.Count < _easyCount)
+        {
+            int easyRandom;
+
+            do
+            {
+                easyRandom = Random.Range(0, _easyCount);
+            } while (usedEasyQuestions.Contains(easyRandom));
+
+            usedEasyQuestions.Add(easyRandom);
 
             return quizlistdata.easy[easyRandom];
         }
@@ -72,9 +91,10 @@ public class Quiz : MonoBehaviour
 
     }
 
-    public void ShowEasyQuiz()
+    public void ShowEasyQuiz() // 퀴즈가 보이게 함.
     {
         QuizData easyQuiz = GETEasyQuiz();
+
         Debug.Log(easyQuiz.quizNum);
         Debug.Log(easyQuiz.category);
         Debug.Log(easyQuiz.quiz);
@@ -82,13 +102,51 @@ public class Quiz : MonoBehaviour
         Debug.Log(easyQuiz.answer);
         Debug.Log(easyQuiz.desc);
 
-        TextquizNum.text = $"{easyQuiz.quizNum}";
-        Textcategory.text = $"{easyQuiz.category}";
-        Textquiz.text = $"{easyQuiz.quiz}";
+        if (easyQuiz != null)
+        {
+            TextquizNum.text = $"{easyQuiz.quizNum}";
+            Textcategory.text = $"{easyQuiz.category}";
+            Textquiz.text = $"{easyQuiz.quiz}";
+        }
+        else
+        {
+            Debug.Log("그만 틀리세용!");
+        }
     }
-    
-    
+
+    public void OnAnswerSelected(string selectedAnswer)
+    {
+        //현재 어디이썽?
+        if (usedEasyQuestions.Count > 0)
+        {
+            int lastQuestionIndex = usedEasyQuestions[usedEasyQuestions.Count - 1];
+            QuizData currentQuiz = quizlistdata.easy[lastQuestionIndex];
+
+            //정답 체크
+            if (currentQuiz.answer == selectedAnswer)
+            {
+                Debug.Log("정답입니다");
+                correntAnswerCount++;
+
+                if (correntAnswerCount >= 5)
+                {
+                    Debug.Log("정답을 다 맞췄습니다! 노동을 종료합니다!");
+                    sugoimage.SetActive(true);
+                    return;
+                }
+
+            }
+            else
+            {
+                Debug.Log("틀렸습니다.");
+            }
+
+            //다음문제
+            ShowEasyQuiz();
+        }
+    }
 }
 
 // 뽑는 메소드 1
 // 출력하는 메소드 1 나눠리ㅏ!!
+//o, x 버튼이 나누어져 있는데 매 문제에 들어오는 버튼을 answer에 따라 정답인지 아닌지 판단하기.
