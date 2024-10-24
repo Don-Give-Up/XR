6dc5330318bbb0a3e8bd30cb7e75f2baaaf9be26
MinJuuu91923 <qkralswn306@gmail.com>
2024-10-25 05:14:17 +0900

    테스트맵 + 멀티

--- 4Don/Assets/PMJ/Scripts/PlayerSpawner.cs
+++ 4Don/Assets/PMJ/Scripts/PlayerSpawner.cs
@@ -10,6 +10,7 @@ public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
         if (player == Runner.LocalPlayer)
         {
             var obj = Runner.Spawn(PlayerPrefab, new Vector3(226.1f, 47f, 364.8f), Quaternion.identity);
+            //var obj = Runner.Spawn(PlayerPrefab, new Vector3(0f, 0f, 0f), Quaternion.identity);
             //obj.GetComponent<NetworkTransform>().Teleport(new Vector3(465f, 47f, 570f));
             //obj.GetComponent<NetworkTransform>().Teleport(new Vector3(0f, 1.5f, 0f));
             //obj.GetComponent<NetworkTransform>().Teleport(new Vector3(226.1f, 47f, 364.8f));
--- 4Don/Assets/PMJ/Scripts/RaycastAttack.cs
+++ 4Don/Assets/PMJ/Scripts/RaycastAttack.cs
@@ -20,8 +20,8 @@ public class RaycastAttack : NetworkBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
             Debug.DrawRay(ray.origin, ray.direction, Color.red, 1f);
-            
-            if (Runner.GetPhysicsScene().Raycast(ray.origin,ray.direction, out var hit))
+
+            if (Runner.GetPhysicsScene().Raycast(ray.origin, ray.direction, out var hit))
             {
                 if (hit.transform.TryGetComponent<Health>(out var health))
                 {
