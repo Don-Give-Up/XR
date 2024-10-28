ec14f7b8ea77dbe5c1ae522db8229d793c1660a1
MinJuuu91923 <qkralswn306@gmail.com>
2024-10-28 18:26:55 +0900

    퀴즈 2D 맵 적용 완

--- 4Don/Assets/PMJ/Scripts/QuizDoor.cs
+++ 4Don/Assets/PMJ/Scripts/QuizDoor.cs
@@ -1,17 +1,17 @@
+using System;
 using UnityEngine;
 
 public class QuizDoor : MonoBehaviour
 {
     public Quiz a;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("QuizDoor"))
         {
             Debug.Log("노동 문 열어라");
            
-            a.QuizStart();
-            a.ShowEasyQuiz();
-            a.oxCanvas.gameObject.SetActive(true);
+            a.AStart();
         }
     }
 }
