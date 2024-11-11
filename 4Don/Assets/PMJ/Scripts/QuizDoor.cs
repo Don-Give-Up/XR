f48447d6224cda53a95479d4281e2727af498ff8
MinJuuu91923 <qkralswn306@gmail.com>
2024-11-11 14:35:08 +0900

    F1키

--- 4Don/Assets/PMJ/Scripts/QuizDoor.cs
+++ 4Don/Assets/PMJ/Scripts/QuizDoor.cs
@@ -8,20 +8,27 @@ public class QuizDoor : MonoBehaviour
 {
     private PhoStartGame a;
     private bool _interact = true;
-    public GameObject eKey;
+    public GameObject f1Key;
+    //콜라이더 닿았을때를 불타입변수로
+    //그때 f1를 누르기
     
     private void Start()
     {
         a = PhoStartGame.Instance;
     }
 
+    private void Update()
+    {
+        // if(Input.GetKeyDown(KeyCode.f1))
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("닿음");
-        //eKey.SetActive(true);
-        if (other.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.P) && _interact)
+        f1Key.SetActive(true);
+        if (other.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.F1) && _interact)
         {
-            //eKey.SetActive(false);
+            f1Key.SetActive(false);
             _interact = false;
             Debug.Log("노동 문 열어라");
 
