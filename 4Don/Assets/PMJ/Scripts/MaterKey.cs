0754274ca512429a9bc76218b507d104a339f817
MinJuuu91923 <qkralswn306@gmail.com>
2024-11-15 08:30:38 +0900

    돈 마스터키 추가

--- 4Don/Assets/PMJ/Scripts/MaterKey.cs
+++ 4Don/Assets/PMJ/Scripts/MaterKey.cs
@@ -6,6 +6,14 @@ public class MaterKey : MonoBehaviour
    public GameObject newsmain;
    public GameObject chatBot;
    public GameObject blur;
+   private PersonalFinancialManager money;
+
+
+   private void Start()
+   {
+      // PersonalFinancialManager 싱글톤 인스턴스를 가져옵니다.
+      money = PersonalFinancialManager.Instance;
+   }
 
    private void Update()
    {
@@ -14,6 +22,15 @@ public class MaterKey : MonoBehaviour
          newsmain.SetActive(false);
          chatBot.SetActive(false);
          blur.SetActive(false);
+      }
+
+      if (Input.GetKeyDown(KeyCode.Alpha1))
+      {
+         if (money != null)
+         {
+            // InputMoney 메서드를 통해 1키를 눌렀을 때 돈이 추가되도록 호출
+            money.InputMoney(8 * 8590);
+         }
       }
    }
 }
