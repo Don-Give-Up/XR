5b5e583233c0cd39c198531bb2a1eceb1fffc664
MinJuuu91923 <qkralswn306@gmail.com>
2024-11-23 02:51:53 +0900

    퀴즈 수정

--- 4Don/Assets/PMJ/Scripts/Communication/BEQuizSolvePost.cs
+++ 4Don/Assets/PMJ/Scripts/Communication/BEQuizSolvePost.cs
@@ -6,18 +6,15 @@ using UnityEngine.Networking;
 
 public class BEQuizSolvePost : MonoBehaviour
 {
-    public int id; // 클릭시
-    public int qid; // 클릭시
-    public string type; // 클릭시
+ 
     private async void Start()
     {
         if (!GoogleSheetManager.Instance.IsLoaded)
             await UniTask.WaitUntil(() => GoogleSheetManager.Instance.IsLoaded);
-
-        SendQuizSolveRequest();
+        
     }
 
-    public void SendQuizSolveRequest()
+    public void SendQuizSolveData(int quizNum, string correct)
     {
         var urlData = GoogleSheetManager.Instance.UrldataGet("퀴즈풀이기록");
 
@@ -26,28 +23,28 @@ public class BEQuizSolvePost : MonoBehaviour
             Debug.LogError("퀴즈풀이기록 URL의 서버 주소가 비어있습니다.");
             return;
         }
-        /*// 보낼 데이터를 생성
+        
         var quizSolve = new QuizSolve
         {
-            gameId = id,
-            quizId = qid,
+            gameId = GameDataManager.Instance.gameId,
+            quizId = quizNum,
             correct = "CORRECT"
         };
-        */
+      
 
         
-        // 보낼 데이터를 생성
+        /*// 보낼 데이터를 생성
         var quizSolve = new QuizSolve
         {
             gameId = 1,
             quizId = 1,
             correct = "CORRECT"
-        };
+        };*/
 
-        PostTradeRequest(urlData.Server, quizSolve).Forget();
+        PostQuizSolveRequest(urlData.Server, quizSolve).Forget();
     }
 
-    private async UniTask PostTradeRequest(string url, QuizSolve stockRecord)
+    private async UniTask PostQuizSolveRequest(string url, QuizSolve stockRecord)
     {
         // JSON 직렬화
         string jsonBody = JsonConvert.SerializeObject(stockRecord);
@@ -57,9 +54,9 @@ public class BEQuizSolvePost : MonoBehaviour
         request.uploadHandler = new UploadHandlerRaw(bodyRaw);
         request.downloadHandler = new DownloadHandlerBuffer();
         request.SetRequestHeader("Content-Type", "application/json");
-        //request.SetRequestHeader("Authorization", LoginCommunicator.Value);
-        request.SetRequestHeader("Authorization",
-            "Bearer eyJkYXRlIjoxNzMxMTM4NzQxOTcwLCJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiJ0b2tlbiA6IDYiLCJtZW1iZXJTY2hvb2wiOiLsi6DssL3spJEiLCJtZW1iZXJHcmFkZSI6MSwibWVtYmVyTmFtZSI6IuyGoe2YuOynhCIsIm1lbWJlck5pY2tuYW1lIjoi7Iah7Zi47KeEIiwiZXhwIjoxNzYyNjc0NzQxLCJtZW1iZXJSb2xlIjoiVEVBQ0hFUiIsIm1lbWJlckNsYXNzIjoyLCJtZW1iZXJJZCI6NiwibWVtYmVyRW1haWwiOiIwOTE4c3lqQGcuY29tIn0.pH30ziFfTxYDLMqSAfBvKUvfIdEXlRCexcO6zg5ig8k");
+        request.SetRequestHeader("Authorization", LoginCommunicator.Value);
+        //request.SetRequestHeader("Authorization",
+            //"Bearer eyJkYXRlIjoxNzMxMTM4NzQxOTcwLCJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiJ0b2tlbiA6IDYiLCJtZW1iZXJTY2hvb2wiOiLsi6DssL3spJEiLCJtZW1iZXJHcmFkZSI6MSwibWVtYmVyTmFtZSI6IuyGoe2YuOynhCIsIm1lbWJlck5pY2tuYW1lIjoi7Iah7Zi47KeEIiwiZXhwIjoxNzYyNjc0NzQxLCJtZW1iZXJSb2xlIjoiVEVBQ0hFUiIsIm1lbWJlckNsYXNzIjoyLCJtZW1iZXJJZCI6NiwibWVtYmVyRW1haWwiOiIwOTE4c3lqQGcuY29tIn0.pH30ziFfTxYDLMqSAfBvKUvfIdEXlRCexcO6zg5ig8k");
             
 
         await request.SendWebRequest();
