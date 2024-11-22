86ffc18e7d8349155ada7ab2fe2e9ea339d6a594
MinJuuu91923 <qkralswn306@gmail.com>
2024-11-23 03:32:16 +0900

    마이크 수정

--- 4Don/Assets/PMJ/Scripts/VoiceMasterKey.cs
+++ 4Don/Assets/PMJ/Scripts/VoiceMasterKey.cs
@@ -1,41 +1,59 @@
-using System;
 using Photon.Voice.Unity;
 using UnityEngine;
 
 public class VoiceMasterKey : MonoBehaviour
 {
-   public Recorder recorder;
- 
+    public Recorder recorder;
 
-   private void Start()
-   {
-      if (recorder == null)
-      {
-         recorder = FindObjectOfType<Recorder>();
-         Debug.Log("레코더 찾았다");
-      }
-   }
-   private void Update()
-   {
-      if (Input.GetKeyDown(KeyCode.Tab))
-      {
-         if (recorder != null)
-         {
-            recorder.TransmitEnabled = true;
-            Debug.Log("마이크 켜짐 ㅎㅎ");
-         }
-      }
+    private void Start()
+    {
+        if (recorder == null)
+        {
+            recorder = FindObjectOfType<Recorder>();
+            if (recorder != null)
+            {
+                Debug.Log("레코더 찾았다");
+                recorder.TransmitEnabled = false; // 마이크 초기 상태 비활성화
+            }
+            else
+            {
+                Debug.LogWarning("레코더를 찾을 수 없습니다.");
+            }
+        }
+    }
 
-      if (Input.GetKeyUp(KeyCode.Tab))
-      {
-         if (recorder != null)
-         {
+    private void LateUpdate()
+    {
+        if (recorder != null && recorder.TransmitEnabled && !Input.GetKey(KeyCode.Tab))
+        {
             recorder.TransmitEnabled = false;
-            Debug.Log("마이크 꺼짐 ㅠㅠ");
-         }
-      }
+            Debug.Log("마이크 초기화: 비활성화 완료");
+        }
+    }
 
-      
-   }
-   
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            Debug.Log("Tab 키 눌림 상태");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (recorder != null)
+            {
+                recorder.TransmitEnabled = true;
+                Debug.Log("마이크 켜짐 ㅎㅎ");
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            if (recorder != null)
+            {
+                recorder.TransmitEnabled = false;
+                Debug.Log("마이크 꺼짐 ㅠㅠ");
+            }
+        }
+    }
 }
