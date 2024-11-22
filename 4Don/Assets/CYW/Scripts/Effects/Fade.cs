d0bbfa007a54f61b6860add02d6e66bbcf23c7bd
ChoYeoWon1 <yw280yw@gmail.com>
2024-11-22 10:58:23 +0900

    문 수정 NPC 씬 수정

---
+++ 4Don/Assets/CYW/Scripts/Effects/Fade.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    
+    /// 플레이어가 콜라이더에 닿으면 FadeIn하고 바로 FadeOut돼야 함
+    /// PostProcessing에 있는 Vignette를 이용해서 동그랗게 모여지며 FadeIn,
+    /// 검은화면이 되면 바로 동그랗게 FadeOut
+    
+    
+    
+    
+    
+    
+}
