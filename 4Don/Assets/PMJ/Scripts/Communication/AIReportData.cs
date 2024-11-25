64d8441f5349aa0921870adff555a95a3e46c49c
MinJuuu91923 <qkralswn306@gmail.com>
2024-11-25 21:11:42 +0900

    레포트쪽 수정

--- 4Don/Assets/PMJ/Scripts/Communication/AIReportData.cs
+++ 4Don/Assets/PMJ/Scripts/Communication/AIReportData.cs
@@ -3,43 +3,45 @@ using Newtonsoft.Json;
 [System.Serializable]
 public class ResponseData
 {
-   
-    public RawData raw_Data;
-    public string analysis;
+    public RawData raw_data; // JSON의 "raw_data" 키와 매핑
+    public Analysis analysis; // JSON의 "analysis" 키와 매핑
 }
 
 [System.Serializable]
 public class RawData
 {
-    public int player_id;
-    public Assets assets;
-    public Ratios ratios;
-    public WrongAnswer[] wrong_answers;
+    public int player_id; // 플레이어 ID
+    public Assets assets; // 자산 정보
+    public Ratios ratios; // 비율 정보
 }
 
 [System.Serializable]
 public class Assets
 {
-    public float total;
-    public float cash;
-    public float savings;
-    public float products;
-    public float stocks;
+    public float total; // 총 자산
+    public float cash; // 현금
+    public float savings; // 저축
+    public float products; // 상품
+    public float stocks; // 주식
+    public float avg_difference; // 평균 대비 금액
 }
 
 [System.Serializable]
 public class Ratios
 {
-    public float cash;
-    public float savings;
-    public float products;
-    public float stocks;
+    public float cash; // 현금 비율
+    public float savings; // 저축 비율
+    public float products; // 상품 비율
+    public float stocks; // 주식 비율
 }
 
 [System.Serializable]
-public class WrongAnswer
+public class Analysis
 {
-    public string quiz;
-    public string answer;
-    public string desc;
+    
+    public string AssetStatusSummary; // 자산 현황 요약
+    public string AssetManagementStatus; // 자산 운용 현황
+    public string InvestmentPropensityAnalysis; // 투자 성향 분석
+    public string LearningAnalytics; // 금융 이해도 진단
+    public string ImprovementSuggestions; // 개선 제안
 }
