302ea120d4f47ee7010158a4e48e8937230df99d
YUJINDL01 <qkrdbwlsdl01@gmail.com>
2024-11-26 22:12:33 +0900

    QuestPosition

--- 4Don/Assets/PYJ/Scripts/QuestPosition.cs
+++ 4Don/Assets/PYJ/Scripts/QuestPosition.cs
@@ -1,7 +1,16 @@
+using System;
 using UnityEngine;
 
 public class QuestPosition : MonoBehaviour
 {
-    public string pointName; 
+    public string pointName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            QuestPositionManager.instance.isTouched = true; 
+        }
+    }
 }
 
