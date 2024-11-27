53277ecdcd5c3468021643fd36102f8f2f839198
YUJINDL01 <qkrdbwlsdl01@gmail.com>
2024-11-27 22:01:57 +0900

    QuestComplete

--- 4Don/Assets/CYW/Scripts/Button/MenuButtonHandler.cs
+++ 4Don/Assets/CYW/Scripts/Button/MenuButtonHandler.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class MenuButtonHandler : MonoBehaviour
+public class MenuButtonHandler : MonoBehaviour // 
 {
     public MenuButtonController menuButtonController;
 
